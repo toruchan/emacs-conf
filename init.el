63412f0e075df5e3521016cfbcf183bc13cfac44
toruchan <ishikawatoru01@gmail.com>
2020-02-13 15:23:49 +0900

    ruby-mode,js-mode追加

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
 (package-initialize)
 
 (require 'package)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")))
+
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
 
@@ -18,10 +23,25 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (company-go go-mode rjsx-mode markdown-mode yaml-mode slim-mode php-mode init-loader auto-complete))))
+    (auto-correct robe flycheck company-go go-mode rjsx-mode markdown-mode yaml-mode slim-mode php-mode init-loader auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; flycheck
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (setq flycheck-checker 'ruby-rubocop)
+             (flycheck-mode 1)))
+(defun ruby-mode-set-encoding () nil)
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ruby-insert-encoding-magic-comment nil) 
+
+(add-hook 'js-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
